2037020c18cb3aef12174e7836f5827031f5ad6d
Nick Starkov <phpnick.ru@gmail.com>
2022-05-18 13:07:12 +0500

    Redesign Form View

--- resources/views/cards/form.blade.php
+++ resources/views/cards/form.blade.php
@@ -1,6 +1,6 @@
 @inject('regions', 'App\Http\Utilities\Region')
 
-<form method="POST" enctype="multipart/form-data" action="/cards" class="d-grid gap-2 col-md-6 dropzone">
+<form method="POST" enctype="multipart/form-data" action="/cards" class="dropzone">
 
 	@if (count($errors) > 0)
 
@@ -16,61 +16,71 @@
 
 	@endif
 
-	{{ csrf_field() }}
-	
-	<div class="form-group">
-		<label for="region">Регион:</label>
-		<select name="region" id="region" class="form-control" required>
+	<div class="row">
+
+		<div class="col-md-6 d-grid gap-2">
+			{{ csrf_field() }}
 			
-			@foreach ($regions::all() as $id => $region)
-			<option value="{{ $id }}">{{ $region }}</option>
-			@endforeach
+			<div class="form-group">
+				<label for="region">Регион:</label>
+				<select name="region" id="region" class="form-control" required>
+					
+					@foreach ($regions::all() as $id => $region)
+					<option value="{{ $id }}">{{ $region }}</option>
+					@endforeach
 
-		</select>
-	</div>
+				</select>
+			</div>
 
-	<div class="form-group">
-		<label for="city">Город:</label>
-		<input type="text" name="city" id="city" class="form-control" value="{{ old('city') }}" required>
-	</div>
+			<div class="form-group">
+				<label for="city">Город:</label>
+				<input type="text" name="city" id="city" class="form-control" value="{{ old('city') }}" required>
+			</div>
 
-	<div class="form-group">
-		<label for="street">Улица:</label>
-		<input type="text" name="street" id="street" class="form-control" value="{{ old('street') }}" required>
-	</div>		
+			<div class="form-group">
+				<label for="street">Улица:</label>
+				<input type="text" name="street" id="street" class="form-control" value="{{ old('street') }}" required>
+			</div>		
 
-	<div class="form-group">
-		<label for="zip">Индекс:</label>
-		<input type="text" name="zip" id="zip" class="form-control" value="{{ old('zip') }}" required>
-	</div>			
+			<div class="form-group">
+				<label for="zip">Индекс:</label>
+				<input type="text" name="zip" id="zip" class="form-control" value="{{ old('zip') }}" required>
+			</div>			
 
-	<div class="form-group">
-		<label for="price">Цена продажи:</label>
-		<input type="text" name="price" id="price" class="form-control" value="{{ old('price') }}" required>
-	</div>
+			<div class="form-group">
+				<label for="price">Цена продажи:</label>
+				<input type="text" name="price" id="price" class="form-control" value="{{ old('price') }}" required>
+			</div>
 
-	<div class="form-group">
-		<label for="photo">Фото:</label>
-		<input type="file" 
-	    class="filepond"
-	    name="photo" 
-	    accept="image/png, image/jpeg, image/jpg, image/bmp"
-	    multiple 
-	    data-allow-reorder="true"
-	    data-max-file-size="3MB"
-	    data-max-files="3">
+			<div class="form-group">
+				<label for="description">Описание:</label>
+				<textarea type="text" name="description" id="description" class="form-control" rows="5" required>{{ old('description') }}</textarea>
+			</div>				
+		</div>
+
+		<div class="col-md-6 d-grid gap-2">
+			<div class="form-group">
+				<label for="photo">Фото:</label>
+				<input type="file" 
+			    class="filepond"
+			    name="photo" 
+			    accept="image/png, image/jpeg, image/jpg, image/bmp"
+			    multiple 
+			    data-allow-reorder="true"
+			    data-max-file-size="3MB"
+			    data-max-files="3">
+			</div>				
+		</div>
+
+		<div class="col-md-12 d-grid gap-2 my-2">
+			<div class="form-group">
+				<button type="submit" class="btn btn-info">
+					Разместить объявление
+				</button>
+			</div>			
+		</div>
+
 	</div>	
-
-	<div class="form-group">
-		<label for="description">Описание:</label>
-		<textarea type="text" name="description" id="description" class="form-control" rows="10" required>{{ old('description') }}</textarea>
-	</div>
-
-	<div class="form-group">
-		<button type="submit" class="btn btn-info">
-			Разместить объявление
-		</button>
-	</div>										
 
 </form>	
 
