a2969eaa41bf54cb367b876226fece0725b8d240
Nick Starkov <phpnick.ru@gmail.com>
2022-05-20 22:39:38 +0500

    Routes Fix

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CardsController;
 use App\Http\Controllers\PagesController;
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/', [CardsController::class, 'index']);
 
 Route::resource('cards', CardsController::class);
@@ -11,7 +15,3 @@ Route::resource('cards', CardsController::class);
 Route::get('{id}', [CardsController::class, 'show']);
 
 Route::post('photos', [CardsController::class, 'uploadPhotos']);
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
